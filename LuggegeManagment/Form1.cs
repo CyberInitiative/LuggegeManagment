644f153fdd5ff07f9b51658d05a6bcab8f7fdc2e
Мирослав Левдиков <deadstronghold@gmail.com>
2019-05-23 23:45:08 +0300

    Add project files.

---
+++ LuggegeManagment/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuggegeManagment
+{
+    public partial class Form1 : Form
+    {
+        List<Passenger> passengers = new List<Passenger>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Passenger p1 = new Passenger("Ivanko", "Petr", "Petrovich", "CK101", "CH100433", 1, 11.2);
+            Passenger p2 = new Passenger("Petrenko", "Oleg", "Ivanovich", "SK1212", "CK123412", 1, 12.5);
+            Passenger p3 = new Passenger("Sidorov", "Ivan", "Ivanovich", "JK302", "SL12121", 2, 5);
+            passengers.Add(p1);
+            passengers.Add(p2);
+            passengers.Add(p3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LuggegeManagment/Passanger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuggegeManagment
+{
+    class Passenger
+    {
+        public string surname;
+        public string name;
+        public string patronymic;
+        public string flightnumber;
+        public string laggagenumber;
+        public int amountofplaces;
+        public double sumweight;
+
+        public Passenger(string s, string n, string p, string f, string l, int a, double sum)
+        {
+            surname = s;
+            name = n;
+            patronymic = p;
+            flightnumber = f;
+            laggagenumber = l;
+            amountofplaces = a;
+            sumweight = sum;
+        }
+    }
+}
